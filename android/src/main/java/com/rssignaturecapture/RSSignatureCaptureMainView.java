5a5e5755ac94b2d0c353a58adb789bd92574dd48
Igor Goremykin <leg3nd1991@gmail.com>
2021-03-12 19:32:18 +0200

    Update RSSignatureCaptureMainView.java

--- android/src/main/java/com/rssignaturecapture/RSSignatureCaptureMainView.java
+++ android/src/main/java/com/rssignaturecapture/RSSignatureCaptureMainView.java
@@ -19,7 +19,6 @@ import android.util.Base64;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
